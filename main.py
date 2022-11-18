3b25a92cc9edab5b986722ecf149b1c613f001fb
Ksenia Likhosherstova <ksenia.likhosherstova@gmail.com>
2022-11-18 11:55:33 +0300

    deleting unnecessary files

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template, request, redirect, url_for
+
+from model import db, db_init, Post
+from config import SQLITE_DATABASE_NAME
+
+app = Flask(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + SQLITE_DATABASE_NAME
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+db.app = app
+db.init_app(app)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about.html')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/resume.html')
+def resume():
+    return render_template("resume.html")
+
+
+@app.route('/contact.html', methods=['GET', 'POST'])
+def message():
+    posts = Post.query.order_by(Post.id.desc()).all()
+
+    if request.method == "POST":
+        name = request.form.get('name', type=str, default="")
+        text = request.form.get('text', type=str, default="")
+        print(name, text)
+        p = Post(name=name, text=text)
+        db.session.add(p)
+        db.session.commit()
+        return redirect(url_for('message'))
+
+    return render_template("contact.html", posts=posts)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "init":
+            with app.app_context():
+                db_init()
+                sys.exit(0)
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
