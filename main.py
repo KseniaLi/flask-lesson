a215c7d4e7c6c018da49e2ba905b1b5c0914c6b0
Ksenia Likhosherstova <ksenia.likhosherstova@gmail.com>
2022-11-18 09:28:26 +0300

    adding .gitignore

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Simple flask app</h1>"
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
 
